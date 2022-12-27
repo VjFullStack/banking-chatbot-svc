2549dab30182caaee4fd23f6f7bfbf43f4fd621e
vjfullstack <viraj.salokhe@gmail.com>
2022-12-27 13:34:30 +0530

    initial commit

--- src/main/java/com/banking/chatbot/api/AccountsApiController.java
+++ src/main/java/com/banking/chatbot/api/AccountsApiController.java
@@ -2,12 +2,11 @@ package com.banking.chatbot.api;
 
 import com.banking.chatbot.model.AccountsBody;
 import com.banking.chatbot.model.InlineResponse201;
+import com.banking.chatbot.service.AccountCreationService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.media.Schema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,24 +26,35 @@ public class AccountsApiController implements AccountsApi {
 
     private final HttpServletRequest request;
 
+    @Autowired
+    private AccountCreationService accountCreationService;
+
     @org.springframework.beans.factory.annotation.Autowired
     public AccountsApiController(ObjectMapper objectMapper, HttpServletRequest request) {
         this.objectMapper = objectMapper;
         this.request = request;
     }
 
-    public ResponseEntity<InlineResponse201> accountsPost(@Parameter(in = ParameterIn.DEFAULT, description = "", schema=@Schema()) @Valid @RequestBody AccountsBody body) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<InlineResponse201>(objectMapper.readValue("{\n  \"message\" : \"success\",\n  \"account\" : {\n    \"open_date\" : \"2000-01-23\",\n    \"number\" : \"number\",\n    \"type\" : \"deposit\"\n  }\n}", InlineResponse201.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<InlineResponse201>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
 
-        return new ResponseEntity<InlineResponse201>(HttpStatus.NOT_IMPLEMENTED);
+    /**
+     * Creates a new account in the database.
+     *
+     * @param body the request body containing the account information
+     * @return a response with the created account and a success message, or an error status if something went wrong
+     */
+    public ResponseEntity<InlineResponse201> createAccount(@Valid @RequestBody AccountsBody body) {
+        try {
+            accountCreationService.createAccount(body);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(objectMapper.readValue("{\n  \"message\" : \"success\",\n  \"account\" : {\n    \"open_date\" : \"2000-01-23\",\n    \"number\" : \"number\",\n    \"type\" : \"deposit\"\n  }\n}", InlineResponse201.class));
+        } catch (IOException e) {
+            log.error("Couldn't serialize response for content type application/json", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
+    @Override
+    public ResponseEntity<InlineResponse201> accountsPost(AccountsBody body) {
+        return null;
+    }
 }
